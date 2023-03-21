9ff1fcb9a2e0ff516c3a004609a581b86fbe0e5f
jaehyeon2 <wol5971@gmail.com>
2023-03-21 15:47:22 +0900

    bfs&dfs

---
+++ dfs&bfs/5-3.py
@@ -0,0 +1,52 @@
+# n, m = map(int, input().split())
+
+# graph=[]
+# for i in range(n):
+#     graph.append(list(map(int, input())))
+
+# def dfs(x, y):
+#     if x<=-1 or x>=n or y<=-1 or y>=m:
+#         return False
+#     if graph[x][y]==0:
+#         graph[x][y]=1
+#         dfs(x-1, y)
+#         dfs(x, y-1)
+#         dfs(x+1, y)
+#         dfs(x, y+1)
+#         return True
+#     return False
+
+# count=0
+
+# for i in range(n):
+#     for j in range(m):
+#         if dfs(i, j)==True:
+#             count+=1
+
+# print(count)
+
+n, m = map(int, input().split())
+d=[]
+for i in range(n):
+    d.append(list(map(int, input())))
+
+def dfs(x, y):
+    if x<=-1 or x>=n or y<=-1 or y>=m:
+        return False
+    if d[x][y]==0:
+        d[x][y]=1
+        dfs(x-1, y)
+        dfs(x+1, y)
+        dfs(x, y-1)
+        dfs(x, y+1)
+        return True
+    return False
+
+count=0
+
+for i in range(n):
+    for j in range(m):
+        if dfs(i, j)==True:
+            count+=1
+
+print(count)
